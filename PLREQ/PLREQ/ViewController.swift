68f6c608808e3deeced3b3797dfd97cdcc1475b3
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-09-20 20:55:07 +0900

    :tada: Start Project

---
+++ PLREQ/PLREQ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PLREQ
+//
+//  Created by 이주화 on 2022/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
