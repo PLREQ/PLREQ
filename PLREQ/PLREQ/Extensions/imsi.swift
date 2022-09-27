c80dea3a1b6d10e82385e3699ec2fce7c4bd06fb
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-09-27 20:46:10 +0900

    [Chore] File Structure

---
+++ PLREQ/PLREQ/Extensions/imsi.swift
@@ -0,0 +1,8 @@
+//
+//  imsi.swift
+//  PLREQ
+//
+//  Created by 이주화 on 2022/09/27.
+//
+
+import Foundation
---
+++ PLREQ/PLREQ/Views/PlayListDetailView/PlayListDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlayListDetailViewController.swift
+//  PLREQ
+//
+//  Created by 이성노 on 2022/09/27.
+//
+
+import UIKit
+
+class PlayListDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
