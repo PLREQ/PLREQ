3c4bc94e90957c22b999ac0ead63949f868be670
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-10-05 00:32:57 +0900

    [Feat] #4 - MatchView Layout 구현

---
+++ PLREQ/PLREQ/Views/MatchView/MatchMusicCell.swift
@@ -0,0 +1,15 @@
+//
+//  MatchMusicCell.swift
+//  PLREQ
+//
+//  Created by 이영준 on 2022/10/01.
+//
+
+import UIKit
+
+class MatchMusicCell: UICollectionViewCell {
+    
+    @IBOutlet weak var musicTitle: UILabel!
+    @IBOutlet weak var musicArtist: UILabel!
+    @IBOutlet weak var musicImage: UIImageView!
+}
