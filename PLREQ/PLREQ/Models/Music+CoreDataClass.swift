2737c78317cd92cb16580a0e6cc64f423f4a9801
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-10-08 00:21:41 +0900

    [Feat] #7 Coredata 추가

---
+++ PLREQ/PLREQ/Models/Music+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Music+CoreDataClass.swift
+//  PLREQ
+//
+//  Created by 이주화 on 2022/10/08.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Music)
+public class Music: NSManagedObject {
+
+}
---
+++ PLREQ/PLREQ/Models/Music+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Music+CoreDataProperties.swift
+//  PLREQ
+//
+//  Created by 이주화 on 2022/10/08.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Music {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Music> {
+        return NSFetchRequest<Music>(entityName: "Music")
+    }
+
+    @NSManaged public var artist: String?
+    @NSManaged public var musicImageURL: URL?
+    @NSManaged public var title: String?
+
+}
+
+extension Music : Identifiable {
+
+}
