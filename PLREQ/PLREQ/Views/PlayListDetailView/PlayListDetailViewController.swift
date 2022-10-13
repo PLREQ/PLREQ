995a949eee1376c57c70cd7505e720f23d875df4
LeeSungNo-ian <isnn0103@gmail.com>
2022-10-14 01:50:17 +0900

    [Feat] #22 PlayList 를 보여주는 PlayListDetailView 구현
    - 저장된 PlayList를 자세히 보여주는 PlayListDetailView 를 구현합니다.

--- PLREQ/PLREQ/Views/PlayListDetailView/PlayListDetailViewController.swift
+++ PLREQ/PLREQ/Views/PlayListDetailView/PlayListDetailViewController.swift
@@ -7,7 +7,11 @@
 
 import UIKit
 
-class PlayListDetailViewController: UIViewController {
+final class PlayListDetailViewController: UIViewController {
+    
+    @IBOutlet weak var shareButtonTapped: UIButton!
+    @IBOutlet weak var musicDetailTableView: UITableView!
+    
     var playList: PlayListDB!
     var musicList: [MusicDB]! {
         return playList.music?.array as? [MusicDB]
@@ -16,17 +20,33 @@ class PlayListDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        musicDetailTableView.delegate = self
+        musicDetailTableView.dataSource = self
+        
+        shareButtonTapped.layer.cornerRadius = shareButtonTapped.frame.height / 2
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func shareButtonTapped(_ sender: Any) {
+        print("되나?")
     }
-    */
+}
 
+extension PlayListDetailViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return musicList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "playListDetailCell", for: indexPath) as? PlayListDetailCell
+        
+        let musicData = musicList[indexPath.row]
+        cell?.musicImage.load(url: musicData.dataToURL(forKey: "musicImageURL"))
+        cell?.musicImage.layer.cornerRadius = 6
+//        view.layer.cornerRadius = Constant.thumbnailSize / 2.0
+
+        cell?.musicTitle.text = musicData.dataToString(forKey: "title")
+        cell?.musicArtist.text = musicData.dataToString(forKey: "artist")
+        
+        return cell ?? UITableViewCell()
+    }
 }
