d51cfe3057c4919c6beacfbcc7cf8717019d90e4
yeni <dev.yeeun@gmail.com>
2022-10-25 00:49:50 +0900

    [Feat] #40 - Custom Annotation View 추가 및 목업 연결

---
+++ PLREQ/PLREQ/Views/MapView/CustomAnnotationView.swift
@@ -0,0 +1,144 @@
+//
+//  CustomAnnotationView.swift
+//  PLREQ
+//
+//  Created by Yeni Hwang on 2022/10/24.
+//  Reference https://developer.apple.com/documentation/mapkit/mapkit_annotations/annotating_a_map_with_custom_data
+
+import UIKit
+import MapKit
+
+class CustomAnnotationView: MKAnnotationView {
+
+    private let boxInset = CGFloat(10)
+//    private let interItemSpacing = CGFloat(10)
+    private let maxContentWidth = CGFloat(90)
+    private let contentInsets = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+    
+    private let blurEffect = UIBlurEffect(style: .systemThickMaterial)
+    private lazy var backgroundMaterial: UIVisualEffectView = {
+        let view = UIVisualEffectView(effect: blurEffect)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [/*labelVibrancyView, */imageView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .top
+//        stackView.spacing = interItemSpacing
+        
+        return stackView
+    }()
+        
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: nil)
+        return imageView
+    }()
+    
+    private var imageHeightConstraint: NSLayoutConstraint?
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = UIColor.clear
+        addSubview(backgroundMaterial)
+
+        backgroundMaterial.contentView.addSubview(stackView)
+        
+        // Make the background material the size of the annotation view container
+        backgroundMaterial.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        backgroundMaterial.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        backgroundMaterial.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        backgroundMaterial.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        
+        // Anchor the top and leading edge of the stack view to let it grow to the content size.
+        stackView.leadingAnchor.constraint(equalTo: backgroundMaterial.leadingAnchor, constant: contentInsets.left).isActive = true
+        stackView.topAnchor.constraint(equalTo: backgroundMaterial.topAnchor, constant: contentInsets.top).isActive = true
+        
+        // Limit how much the content is allowed to grow.
+        imageView.widthAnchor.constraint(equalToConstant: maxContentWidth).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+    override func prepareForDisplay() {
+        super.prepareForDisplay()
+        
+        if let annotation = annotation as? CustomAnnotation {
+            
+            if let imageName = annotation.imageName, let image = UIImage(named: imageName) {
+                imageView.image = image
+                
+                /*
+                 The image view has a width constraint to keep the image to a reasonable size. A height constraint to keep the aspect ratio
+                 proportions of the image is required to keep the image packed into the stack view. Without this constraint, the image's height
+                 will remain the intrinsic size of the image, resulting in extra height in the stack view that is not desired.
+                 */
+                
+                if let heightConstraint = imageHeightConstraint {
+                    imageView.removeConstraint(heightConstraint)
+                }
+                
+                let ratio = image.size.height / image.size.width
+                imageHeightConstraint = imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: ratio, constant: 0)
+                imageHeightConstraint?.isActive = true
+            }
+        }
+        
+        // Since the image and text sizes may have changed, require the system do a layout pass to update the size of the subviews.
+        setNeedsLayout()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // The stack view will not have a size until a `layoutSubviews()` pass is completed. As this view's overall size is the size
+        // of the stack view plus a border area, the layout system needs to know that this layout pass has invalidated this view's
+        // `intrinsicContentSize`.
+        invalidateIntrinsicContentSize()
+        
+        // Use the intrinsic content size to inform the size of the annotation view with all of the subviews.
+        let contentSize = intrinsicContentSize
+        frame.size = intrinsicContentSize
+        
+        // The annotation view's center is at the annotation's coordinate. For this annotation view, offset the center so that the
+        // drawn arrow point is the annotation's coordinate.
+        centerOffset = CGPoint(x: contentSize.width / 2, y: contentSize.height / 2)
+        
+        let shape = CAShapeLayer()
+        let path = CGMutablePath()
+
+        // Draw the pointed shape.
+        let pointShape = UIBezierPath()
+        pointShape.move(to: CGPoint(x: boxInset, y: 0))
+        pointShape.addLine(to: CGPoint.zero)
+        pointShape.addLine(to: CGPoint(x: boxInset, y: boxInset))
+        path.addPath(pointShape.cgPath)
+
+        // Draw the rounded box.
+        let box = CGRect(x: boxInset, y: 0, width: self.frame.size.width - boxInset, height: self.frame.size.height)
+        let roundedRect = UIBezierPath(roundedRect: box,
+                                       byRoundingCorners: [.topRight, .bottomLeft, .bottomRight],
+                                       cornerRadii: CGSize(width: 5, height: 5))
+        path.addPath(roundedRect.cgPath)
+
+        shape.path = path
+        backgroundMaterial.layer.mask = shape
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        var size = stackView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        size.width += contentInsets.left + contentInsets.right
+        size.height += contentInsets.top + contentInsets.bottom
+        return size
+    }
+}
